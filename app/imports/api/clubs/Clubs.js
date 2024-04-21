4214f459d1e2e3997c94a9a309743693212ffea4
Chester Leoso <77557385+cleoso@users.noreply.github.com>
2024-04-20 14:00:40 -1000

    update
    updated schema to include the club type or interests tabular.

--- app/imports/api/clubs/Clubs.js
+++ app/imports/api/clubs/Clubs.js
@@ -14,6 +14,8 @@ class ClubsCollection {
       homepage: { type: String, optional: true },
       description: { type: String, optional: true },
       contact: { type: String, optional: true },
+      // eslint-disable-next-line max-len
+      interest: { type: String, allowedValues: ['Academic/Professional', 'Ethnic/Cultural', 'Fraternity/Sorority', 'Honorary Society', 'Leisure/Recreational', 'Political', 'Religious/Spiritual', 'Service', 'Sports/Leisure', 'Student Affairs'], defaultValue: ' ' },
     });
     // Ensure collection documents obey schema.
     this.collection.attachSchema(this.schema);
